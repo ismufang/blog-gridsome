7ef31280fa64a40d2c87defce80f07bb9a254c73
fanghao <fanghao@fanghaodeMacBook-Pro.local>
2020-12-01 09:59:35 +0800

    f

--- src/main.js
+++ src/main.js
@@ -1,9 +1,21 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
+import DefaultLayout from '~/layouts/Layout.vue'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import 'mavon-editor/dist/markdown/github-markdown.min.css'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
+  Vue.use(Vant)
+  Vue.use(ElementUI)
+  Vue.use(mavonEditor)
   Vue.component('Layout', DefaultLayout)
 }
